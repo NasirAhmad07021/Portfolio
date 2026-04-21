33a67435ae701dfd116b9f583f13f3a7a4547d76
Nasir Ahmad <nasirahmadniazai07021@gmail.com>
2026-04-21 19:07:49 +0430

    I will start making my portfolio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-2xl bg-teal-800'>
+        Hello
+      </div>
+        
+    </>
+  )
+}
+
+export default App
