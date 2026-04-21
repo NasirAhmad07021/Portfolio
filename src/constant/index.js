535c18333b5d8ab04092b02500af537a00f5ef41
Nasir Ahmad <nasirahmadniazai07021@gmail.com>
2026-04-21 19:57:03 +0430

    Portfolio has already finished

---
+++ src/constant/index.js
@@ -0,0 +1,273 @@
+import { BsDropbox, BsInstagram, BsWhatsapp } from "react-icons/bs";
+import { TbBrandNetflix } from "react-icons/tb";
+import { FaShopify } from "react-icons/fa";
+import {
+  bootstrap,
+  brainwave,
+  certificate1,
+  css1,
+  dental,
+  figma,
+  hoobank,
+  html,
+  jquery,
+  js1,
+  lightportfolio,
+  portfolio,
+  react,
+  tailwind,
+  ts1,
+  user1,
+  user2,
+  user3,
+  user4,
+  virtualr,
+  FAQ,
+  webheader,
+  authform,
+} from "../assets/images";
+import {
+  MdApi,
+  MdBugReport,
+  MdDeveloperMode,
+  MdSettings,
+} from "react-icons/md";
+import { SiFrontendmentor } from "react-icons/si";
+import { CgPerformance } from "react-icons/cg";
+
+export const Companies_logos = [
+  {
+    icon: BsDropbox,
+    lable: "DropBox",
+  },
+  {
+    icon: TbBrandNetflix,
+    lable: "Netflex",
+  },
+  {
+    icon: BsInstagram,
+    lable: "Instagram",
+  },
+  {
+    icon: FaShopify,
+    lable: "Shopify",
+  },
+  {
+    icon: BsWhatsapp,
+    lable: "Whatsapp",
+  },
+];
+
+export const tools = [
+  {
+    icon: figma,
+    name: "Figma",
+    usage: "Design tool",
+  },
+  {
+    icon: html,
+    name: "Html",
+    usage: "Web Structure",
+  },
+  {
+    icon: css1,
+    name: "CSS",
+    usage: "User Interface",
+  },
+  {
+    icon: bootstrap,
+    name: "Bootstrap",
+    usage: "css Framework",
+  },
+  {
+    icon: js1,
+    name: "JavaScript",
+    usage: "Interaction",
+  },
+  {
+    icon: ts1,
+    name: "TypeScript",
+    usage: "Strong js",
+  },
+  {
+    icon: tailwind,
+    name: "Tailwind",
+    usage: "css Framework",
+  },
+  {
+    icon: react,
+    name: "React",
+    usage: "js library",
+  },
+  {
+    icon: jquery,
+    name: "JQery",
+    usage: "js library",
+  },
+];
+
+export const ServicesInfo = [
+  {
+    icon: MdDeveloperMode,
+    title: "Web Development",
+    desc: `I build modern, fast, and responsive websites using the latest technologies.
+My workflow is focused on clean code, smooth performance, and scalable structure using react, JavaScript, and Tailwind css.
+`,
+  },
+  {
+    icon: SiFrontendmentor,
+    title: "Front-End UI / UX Design",
+    desc: `I build modern, fast, and responsive websites using the latest technologies.
+My workflow is focused on clean code, smooth performance, and scalable structure using react, JavaScript, and Tailwind css.
+`,
+  },
+  {
+    icon: MdSettings,
+    title: "Component-Based Development",
+    desc: `I build clean, reusable, and scalable components using react.
+This ensures a well-organized codebase and a professional front-end structure.
+`,
+  },
+  {
+    icon: MdBugReport,
+    title: "Optimization & Bug Fixing",
+    desc: `I improve slow websites, fix errors, and optimize UI/UX for better performance.
+Your site will run smoothly, load faster, and feel more professional.
+`,
+  },
+  {
+    icon: MdApi,
+    title: "API Integration",
+    desc: `I integrate REST APIs into websites—
+fetching, displaying, and managing data efficiently while keeping the UI smooth.
+`,
+  },
+  {
+    icon: CgPerformance,
+    title: "Performance Optimization",
+    desc: `Improving Website performance to make it load faster, run, smoother and deliver a better user experience.`,
+  },
+];
+
+export const projects_info = [
+  {
+    darkimage: certificate1,
+    lightimage: certificate1,
+    type: "website",
+    theme: true,
+    title: "React certificate",
+    desc: "A specialized platform designed to guide developers through structured, industry-level react certification preparation.",
+    link: "https://react-certificate1-green.vercel.app/",
+  },
+  {
+    darkimage: hoobank,
+    lightimage: hoobank,
+    type: "website",
+    theme: true,
+    title: "Hoobank",
+    desc: "A fully responsive fintech interface built to showcase seamless digital banking services with a clean user-focused design.",
+    link: "https://hoo-bank-woad.vercel.app/",
+  },
+  {
+    darkimage: brainwave,
+    lightimage: brainwave,
+    type: "website",
+    theme: true,
+    title: "Brainwave",
+    desc: "A modern AI-driven landing page crafted to highlight advanced digital innovation and futuristic user experiences.",
+    link: "https://brain-wave-self.vercel.app/",
+  },
+  {
+    darkimage: virtualr,
+    lightimage: virtualr,
+    type: "website",
+    theme: true,
+    title: "VirtualR",
+    desc: "A visually immersive VR landing page introducing next-generation virtual experiences through elegant, modern UI design.",
+    link: "https://virtual-r-one-kohl.vercel.app/",
+  },
+  {
+    darkimage: dental,
+    lightimage: dental,
+    type: "website",
+    theme: true,
+    title: "Dental",
+    desc: "A professional healthcare website created to present dental services with clarity, trust, and a patient-friendly layout.",
+    link: "https://dental-two-sandy.vercel.app/",
+  },
+  {
+    darkimage: portfolio,
+    lightimage: lightportfolio,
+    type: "website",
+    theme: true,
+    title: "Portfolio",
+    desc: "A personal developer showcase designed to reflect my skills, creativity, and modern web-development craftsmanship.",
+    link: "#",
+  },
+  {
+    darkimage: FAQ,
+    lightimage: FAQ,
+    type: "mini",
+    theme: true,
+    title: "FAQs",
+    desc: "This FAQ component is a fully interactive and reusable built in react, designed to deliver smooth expand-collapse transition and clean structure.",
+    link: "#",
+  },
+  {
+    darkimage: authform,
+    lightimage: authform,
+    type: "mini",
+    theme: true,
+    title: "Authentication Form",
+    desc: "The authentication form is a secure, responsive and validation-driven react feature that handles user login and registration with proper state management.",
+    link: "#",
+  },
+  {
+    darkimage: webheader,
+    lightimage: webheader,
+    type: "mini",
+    theme: true,
+    title: "Website Header",
+    desc: "This website header is a modern, adaptive Header built with react, featuring a clean design, mobile-first responsiveness and interactive menu behavior.",
+    link: "#",
+  },
+];
+
+export const testimonials = [
+  {
+    id: 1,
+    name: "Amine ouali",
+    role: "Software engineer",
+    content:
+      "Nasir consistently demonstrates strong problem-solving abilities and a deep understanding of software principles. His approach to building scalable and clean solutions reflects a level of maturity rare for developers at his stage.",
+    rating: 3,
+    image: user1,
+  },
+  {
+    id: 2,
+    name: "Amine ouali",
+    role: "FrontEnd developer",
+    content:
+      "Nasir has an exceptional eye for detail and user experience. His react and UI work is clean, modern, and highly optimized — always delivering interfaces that feel smooth, intuitive, and professional.",
+    rating: 3,
+    image: user2,
+  },
+  {
+    id: 3,
+    name: "Max Nour",
+    role: "BackEnd developer",
+    content:
+      "Working with Nasir was impressive; he writes structured, secure, and efficient backend logic. His understanding of APIs, data flow, and system architecture makes every collaboration seamless.",
+    rating: 4,
+    image: user3,
+  },
+  {
+    id: 4,
+    name: "Abdullah",
+    role: "FullStack developer",
+    content:
+      "Nasir brings complete ownership to a project — from front-end UI to back-end systems. His ability to align functionality with design and deliver end-to-end solutions makes him a reliable and highly capable full-stack developer.",
+    rating: 5,
+    image: user4,
+  },
+];
